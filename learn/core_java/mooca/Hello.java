41e0c67cfd9f91e33562defa6ab16ddf727af22e
B-TRUNK <abanoub.boshra.anis@gmail.com>
2025-12-17 20:29:04 +0200

    start

--- learn/core_java/mooca/Hello.java
+++ learn/core_java/mooca/Hello.java
@@ -1,5 +1,10 @@
+/**
+*This is a simple Java Program.
+*/
+
 public class Hello {
 
+    // main() is the starting point of your program
     public static void main(String[] args){
 
         System.out.println("Yes, I'm Javon");
